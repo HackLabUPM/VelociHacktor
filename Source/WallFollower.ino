744535fa6193e51397bb4e3afa361248151e8cef
Jg95 <javiergarlo95@gmail.com>
2016-02-21 16:50:58 +0100

    Added files via upload
    This is the first version of the maze solving algorithm using the right wall following method

---
+++ Source/WallFollower.ino
@@ -0,0 +1,46 @@
+int motor1fwd = 5;
+int motor1bck = 6;
+int motor2fwd = 9;
+int motor2bck = 10;
+int sensorfwd = A0;
+int sensorright = A1;
+int dist_min = 10;
+
+void mover(int v1, int v2)
+{
+}
+int leer(int sensor)
+{
+	int lectura, cm;
+	lectura = analogRead (sensor);
+	cm = pow(3027.4 / lectura, 1.2134);
+	return cm;
+}
+
+void setup()
+{
+ 	pinMode (motor1fwd, OUTPUT);
+ 	pinMode (motor1bck, OUTPUT);
+ 	pinMode (motor2fwd, OUTPUT);
+ 	pinMode (motor2bck, OUTPUT);
+ 	Serial.begin(9600);
+
+ 	mover (50, 50);
+}
+
+void loop()
+{
+	if (leer(sensorfwd) < dist_min)
+	{
+		if (leer(sensorright) < dist_min)
+			mover (-50, 50);
+		else
+			mover (50, -50);
+	}
+
+	else if (leer(sensorright) < dist_min)
+		mover (0, 50);
+
+	else if (leer(sensorright) > dist_min)
+		mover (50, 0);
+}
